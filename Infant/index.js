62d5c163541855e5d60bd073ccdc930eb736064a
apurakshit2001 <okritrimoj@gmail.com>
2024-11-10 12:26:13 +0530

    "Deleted Form.css, Form1.html, and Form2.html files"

---
+++ Infant/index.js
@@ -0,0 +1,23 @@
+// Infant Step 1
+
+// There are 3 types of variables in JavaScript: var, let, and const.
+        // var is the old way of declaring variables and it has some quirks.
+        // let and const are the new ways of declaring variables and they are more modern and have better scoping rules.
+        // let and const are block-scoped, which means they are only accessible within the block they are declared in.
+
+        var a = 50;
+        a = 80;
+        console.log(a);
+        let b = 20;
+        b = 30;
+        console.log(b);
+        const c = 40;
+        // c = 50;
+        console.log(c);
+
+        // var a = 50;
+        // var a = 80;
+        // console.log(a);
+        // let b = 20;
+        // let b = 30;
+        // console.log(b);
